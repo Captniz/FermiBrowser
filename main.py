0deb58505d5a560bec9fe2f5774268775f811d87
SimoneTardiani <simone.tardiani@gmail.com>
2023-11-15 10:53:49 +0100

    Added main, First official commit

---
+++ main.py
@@ -0,0 +1,9 @@
+from PyQt5.QtWidgets import *;
+from PyQt5.QtGui import *;
+from PyQt5.QtCore import *;
+from PyQt5.QtWebEngineWidgets import *;
+
+class FermiBrowser(QMainWindow):
+
+    def __init__(self):
+        pass
