b0a296b4448cc9d23fe322e46c91cbac3ffab5c1
SimoneTardiani <simone.tardiani@gmail.com>
2023-11-15 11:22:38 +0100

    Update main

--- main.py
+++ main.py
@@ -1,9 +1,30 @@
-from PyQt5.QtWidgets import *;
-from PyQt5.QtGui import *;
-from PyQt5.QtCore import *;
-from PyQt5.QtWebEngineWidgets import *;
+from PyQt5.QtWidgets import *
+from PyQt5.QtCore import *
+from PyQt5.QtWebEngineWidgets import *
+
 
 class FermiBrowser(QMainWindow):
 
     def __init__(self):
-        pass
+        super(FermiBrowser, self).__init__()
+
+        # LAYOUT
+        self.window = QWidget()
+        self.window.setWindowTitle("Fermi Browser")
+        self.layout = QVBoxLayout()
+        self.horizontal = QHBoxLayout()
+
+        self.browser = QWebEngineView()
+
+        self.layout.addLayout(self.horizontal)
+        self.layout.addWidget(self.browser)
+
+        self.browser.setUrl(QUrl("https://www.google.com"))
+
+        self.window.setLayout(self.layout)
+        self.window.show()
+
+
+app = QApplication([])
+window = FermiBrowser()
+app.exec_()
